32644a5cfcb45ef6d0d0be412a2c8139b8c9bc51
Rishabh-3950 <rishabhy755@gmail.com>
2024-06-02 18:24:02 +0530

    how to intialize array in javascript

---
+++ Array1.js
@@ -0,0 +1,29 @@
+//Array--collection of same data type in a variable
+ //Array literals
+ const fruits=["Mangoes", "Apples","Guava"];
+
+
+ console.log("fruits " ,fruits);
+ 
+ //indexes--access any element inside in array
+ //array follows zero baesd indexing
+ //Access any element with index number
+ console.log(fruits[0]);
+ console.log(fruits[1]);
+ console.log(fruits[2]);
+ 
+ //changed the element in array
+ fruits[0]="grapes";
+ console.log(fruits[0]);
+ 
+ const num=[];
+ num[0]=1;
+ num[1]=2;
+ num[2]=3;
+ console.log("numbers ",num);
+ 
+ 
+ //Array insializing with new keyword
+ const number1=new Array(1,2,3,4,5);
+ console.log("number " , number1);
+ 
