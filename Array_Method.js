688a2a94d4975ff8cb4054fe6983eaf56e14470b
Rishabh-3950 <rishabhy755@gmail.com>
2024-06-02 18:54:41 +0530

    Added some method of Array in javaScript

---
+++ Array_Method.js
@@ -0,0 +1,12 @@
+//methods of Array
+//length method
+const colors=["Red", "Orange","Yellow","Black"];
+console.log(colors.length);
+
+//Array convert to String
+//1.toString() method
+const col=colors.toString();
+console.log(col);
+console.log(typeof(col));
+//2.join() method --you can give operator in this
+console.log(colors.join(" # "));
